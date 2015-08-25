ea9eb9e186285e06fb78f3aab3d4eee45b98eaba
Richard Harrah <topplethenunnery@gmail.com>
2015-08-25 11:57:18 -0400

    adding yardoc to version.rb

--- lib/rgitflow/version.rb
+++ lib/rgitflow/version.rb
@@ -1,3 +1,6 @@
+# Contains all of the modules and classes relevant to this gem.
 module RGitFlow
+  # Represents this gem's current version.
+  # @return [String] current version
   VERSION = "0.1.0"
 end
