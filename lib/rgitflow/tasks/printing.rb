1dec61fcf2fdf01325fcd1b1121f30da45f979e1
Richard Harrah <topplethenunnery@gmail.com>
2015-08-25 20:40:06 -0400

    adding printing module

---
+++ lib/rgitflow/tasks/printing.rb
@@ -0,0 +1,64 @@
+require 'ansi/constants'
+
+module RGitFlow
+  # Contains all of the various tasks relevant to rgitflow.
+  module Tasks
+    # Contains logic for printing messages to the console. Supports ANSI colors.
+    # Inspired by rubygems-tasks' printing.rb
+    module Printing
+      # String to prepend to any status messages
+      # @return [String] prefix for status messages
+      STATUS_PREFIX = if $stdout.tty?
+                        "#{ANSI::Constants::GREEN}#{ANSI::Constants::BRIGHT}>>>#{ANSI::Constants::CLEAR}"
+                      else
+                        '>>>'
+                      end
+
+      # String to prepend to any debug messages
+      # @return [String] prefix for debug messages
+      DEBUG_PREFIX = if $stderr.tty?
+                       "#{ANSI::Constants::YELLOW}#{ANSI::Constants::BRIGHT}>>>#{ANSI::Constants::CLEAR}"
+                     else
+                       '>>>'
+                     end
+
+      # String to prepend to any error messages
+      # @return [String] prefix for error messages
+      ERROR_PREFIX = if $stderr.tty?
+                       "#{ANSI::Constants::RED}#{ANSI::Constants::BRIGHT}>>>#{ANSI::Constants::CLEAR}"
+                     else
+                       '>>>'
+                     end
+
+      protected
+
+      # Prints a status message to the console
+      # @param [String] message message to print to the console
+      # @return [void]
+      def status(message = '')
+        if Rake.verbose
+          $stdout.puts "#{STATUS_PREFIX} #{message}"
+        end
+        nil
+      end
+
+      # Prints a debug message to the console
+      # @param [String] message message to print to the console
+      # @return [void]
+      def debug(message = '')
+        if Rake.verbose
+          $stderror.puts "#{DEBUG_PREFIX} #{message}"
+        end
+        nil
+      end
+
+      # Prints an error message to the console
+      # @param [String] message message to print to the console
+      # @return [void]
+      def error(message = '')
+        $stderror.puts "#{ERROR_PREFIX} #{message}"
+        nil
+      end
+    end
+  end
+end
