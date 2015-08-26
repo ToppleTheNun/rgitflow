032116453d20faa112d91055a528b472e37b5003
Richard Harrah <topplethenunnery@gmail.com>
2015-08-26 00:40:02 -0400

    autoloading printing as well

--- lib/rgitflow/autoload.rb
+++ lib/rgitflow/autoload.rb
@@ -2,5 +2,10 @@
 def __p(path) File.join(RGitFlow::ROOT, 'rgitflow', *path.split('/')); end
 
 module RGitFlow
+  module Tasks
+    autoload :TaskHelper, __p('tasks/task_helpers')
+  end
+
   autoload :Config, __p('config')
+  autoload :Printing, __p('printing')
 end
