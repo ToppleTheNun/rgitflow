b0c33c4442154c2010d5a1b36e201af9eccb4fc7
Richard Harrah <topplethenunnery@gmail.com>
2015-08-26 00:28:22 -0400

    using inspiration from YARD to improve gem

--- lib/rgitflow.rb
+++ lib/rgitflow.rb
@@ -1,2 +1,19 @@
 require 'rgitflow/version'
 require 'rgitflow/tasks/task_helpers'
+
+module RGitFlow
+  # The root path for RGitFlow source libraries
+  ROOT = File.expand_path(File.dirname(__FILE__))
+end
+
+# Load Ruby core extension classes
+Dir.glob(File.join(RGitFlow::ROOT, 'rgitflow', 'core_ext', '*.rb')).each do |file|
+  require file
+end
+
+['autoload', 'globals'].each do |file|
+  require File.join(RGitFlow::ROOT, 'yard', file)
+end
+
+# Load YARD configuration options (and plugins)
+RGitFlow::Config.load
