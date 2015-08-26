caa3297d99e52c46589d857a18939bbfb5bfcf21
Richard Harrah <topplethenunnery@gmail.com>
2015-08-26 12:53:12 -0400

    attempting to delete remote branch

--- lib/rgitflow/tasks/task_helpers.rb
+++ lib/rgitflow/tasks/task_helpers.rb
@@ -86,15 +86,10 @@ module RGitFlow
 
           @git.branch('master').checkout
           @git.merge branch
-          @git.branch(branch).delete
-          for remote in @git.remotes
-            unless remote.branch(branch).nil?
-              debug "Deleting branch #{branch} on remote #{remote.name}"
-              remote.branch(branch).delete
-            end
-          end
-
           @git.push
+          @git.push('origin', branch, {:delete => true})
+
+          @git.branch(branch).delete
 
           status "Finished feature branch #{branch}!"
         end
