8c08e13fea5879ae0e74effc4bd3ab2f9aa62e8d
Richard Harrah <topplethenunnery@gmail.com>
2015-08-26 23:22:09 -0400

    improving definition of dirty for git repo

--- lib/rgitflow/git_ext/base.rb
+++ lib/rgitflow/git_ext/base.rb
@@ -2,8 +2,10 @@ require 'git/base'
 
 Git::Base.class_eval do
   def dirty?
-    puts '0 files changed, 0 insertions(+), 0 deletions(-)'
-    puts diff.diff_shortstat.to_s.strip
-    '0 files changed, 0 insertions(+), 0 deletions(-)' != diff.diff_shortstat.to_s.strip
+    dirt = diff.diff_shortstat.to_s.strip
+    if dirt.blank?
+      true
+    end
+    '0 files changed, 0 insertions(+), 0 deletions(-)' != dirt
   end
 end
