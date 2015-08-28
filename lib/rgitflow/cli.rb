78abd9b1b4953fd5257b4434acf3e063bf27131b
Richard Harrah <topplethenunnery@gmail.com>
2015-08-28 00:00:03 -0400

    adding cli.rb and updating rgitflow to use it

---
+++ lib/rgitflow/cli.rb
@@ -0,0 +1,45 @@
+require 'rgitflow/console'
+require 'rgitflow/printing'
+
+module RGitFlow
+  module CLI
+    include ::RGitFlow::Printing
+    include ::RGitFlow::Console
+
+    class << self
+      attr_accessor :instance
+
+      def execute(command, *arguments)
+        new.execute command, arguments
+      end
+
+      def invoke(name)
+        new.invoke name
+      end
+
+      def task?(name)
+        new.task? name
+      end
+
+      def multi_task(prefix, names)
+        new.multi_task prefix, names
+      end
+
+      def status(message = '')
+        new.status message
+      end
+
+      def debug(message = '')
+        new.debug message
+      end
+
+      def error(message = '')
+        new.error message
+      end
+
+      def prompt(message = '')
+        new.prompt message
+      end
+    end
+  end
+end
